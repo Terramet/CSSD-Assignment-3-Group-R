d1f6ef58a5b51ed945c224687aac16f897bfa48b
Terramet <jbamfo876@gmail.com>
2018-02-26 15:49:59 +0000

    Updating minor things

--- CSSD_Assignment_3/src/cssd_assignment_3/LocationList.java
+++ CSSD_Assignment_3/src/cssd_assignment_3/LocationList.java
@@ -14,9 +14,7 @@ import java.util.List;
  * @author Ascendant
  */
 public class LocationList implements Serializable  {
-    /*
-        Changed from a singleton to being jsut a list 
-    */
+
     private List<Location> locations = null;
     
     public LocationList(List<Location> list){
